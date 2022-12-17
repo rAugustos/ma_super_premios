bd5c7743465c9b0008354ac4df26730a14fc068c
Steve Narancic <58119793+stevenarancic@users.noreply.github.com>
2022-12-17 16:58:25 -0300

    Criado campo is_winner na tabela de números da sorte, para validar quem venceu e quais produtos já foram sorteados.

---
+++ database/factories/ProductImageFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ProductImage>
+ */
+class ProductImageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'product_id' => Product::factory()->create()->id,
+            'path' => fake()->image(public_path('images/products'), 400, 300),
+        ];
+    }
+}
